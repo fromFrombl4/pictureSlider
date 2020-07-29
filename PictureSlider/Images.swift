9c1c88e1d0915cd28ae06dedf7b957527dc418c9
roman dod <stalkeroma1@mail.ru>
2020-07-29 18:05:34 +0300

    create imageView, add images, create custom class

---
+++ PictureSlider/Images.swift
@@ -0,0 +1,23 @@
+//
+//  Images.swift
+//  PictureSlider
+//
+//  Created by Roman Dod on 7/29/20.
+//  Copyright © 2020 Roman Dod. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Images {
+    
+    static let audiImage = UIImage(named: "audi")
+    static let bmwImage = UIImage(named: "bmw")
+    static let ferrariImage = UIImage(named: "ferrari")
+    static let lambaImage = UIImage(named: "lamba")
+    static let mers_clsImage = UIImage(named: "mers_cls")
+    static let rolls_royceImage = UIImage(named: "rolls_royce")
+    static let teslaImage = UIImage(named: "tesla")
+    
+    
+}
--- PictureSlider/ViewController.swift
+++ PictureSlider/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextImageButton: UIButton!
+    @IBOutlet weak var previousImageButton: UIButton!
+    
+    var imagesArray = [UIImage]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imagesArray.append(Images.audiImage!)
+        imagesArray.append(Images.bmwImage!)
+        imagesArray.append(Images.ferrariImage!)
+        imagesArray.append(Images.lambaImage!)
+        imagesArray.append(Images.mers_clsImage!)
+        imagesArray.append(Images.rolls_royceImage!)
+        imagesArray.append(Images.teslaImage!)
+        
+        
     }
 
-
+    @IBAction func nextImageButton(_ sender: UIButton) {
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block:{_ in
+            
+            for i in self.imagesArray{
+            UIView.animate(withDuration: 2, animations: {
+                self.imagesArray[i].layer.position = CGPoint(x: +1000, y: imageView.center)
+                
+            }) { (_) in
+                
+            })
+            
+            
+        })
+        }
+    }
+    @IBAction func previousImageButton(_ sender: UIButton) {
+    }
+    
 }
 
