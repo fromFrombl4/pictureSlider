c6921511cc110302cfd978249027a6eac00714e2
roman dod <stalkeroma1@mail.ru>
2020-07-31 00:44:40 +0300

    create main logic

--- PictureSlider/ViewController.swift
+++ PictureSlider/ViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var nextImageButton: UIButton!
-    @IBOutlet weak var previousImageButton: UIButton!
     
+    enum Direction {
+        case left
+        case right
+    }
+    
+    @IBOutlet weak var imageView: UIImageView?
+    @IBOutlet weak var nextImageButton: UIButton?
+    @IBOutlet weak var previousImageButton: UIButton?
+    
+    var currentImageIndex = 0
+    var durationAnimation = 0.3
+    var delayAnimation = 0.1
     var imagesArray = [UIImage]()
+    var animationInProgress = false
     
     
     override func viewDidLoad() {
@@ -28,27 +38,80 @@ class ViewController: UIViewController {
         imagesArray.append(Images.rolls_royceImage!)
         imagesArray.append(Images.teslaImage!)
         
+        imageView?.image = imagesArray.first
         
     }
 
     @IBAction func nextImageButton(_ sender: UIButton) {
         
-        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block:{_ in
-            
-            for i in self.imagesArray{
-            UIView.animate(withDuration: 2, animations: {
-                self.imagesArray[i].layer.position = CGPoint(x: +1000, y: imageView.center)
-                
-            }) { (_) in
-                
-            })
-            
-            
-        })
+        guard !animationInProgress else {
+            return
         }
+        
+        var nextIndex = currentImageIndex + 1
+        if nextIndex > imagesArray.count - 1 {
+            nextIndex = 0
+        }
+        
+        animateImage(imageIndex: nextIndex, direction: Direction.right)
     }
+    
+    
     @IBAction func previousImageButton(_ sender: UIButton) {
+        guard !animationInProgress else {
+            return
+        }
+        
+        var nextIndex = currentImageIndex - 1
+        if nextIndex < 0 {
+            nextIndex = imagesArray.count - 1
+        }
+         
+        animateImage(imageIndex: nextIndex, direction: Direction.left)
     }
+    
+    private func animateImage(imageIndex: Int, direction: Direction){
+        animationInProgress = true
+        let frame = imageView!.bounds
+        
+        let imgView = UIImageView(frame: frame)
+        imgView.contentMode = .scaleAspectFit
+        imgView.backgroundColor = .white
+        imgView.image = imagesArray[imageIndex]
+        
+        let center = imageView?.center as! CGPoint
+        imgView.center = center
+        switch direction {
+        case .left:
+            imgView.center.x = (imageView?.center.x)! - (imageView?.bounds.width)!
+        case .right:
+            imgView.center.x = (imageView?.center.x)! + (imageView?.bounds.width)!
+        }
+        
+        view.addSubview(imgView)
+        UIView.animate(withDuration: durationAnimation, delay: delayAnimation, options: .curveEaseIn, animations: {
+            
+            imgView.center = center
+            
+        }) {(_) in
+            self.animationInProgress = false
+            
+            switch direction {
+            case .left:
+                self.currentImageIndex -= 1
+                
+                if self.currentImageIndex > self.imagesArray.count - 1{
+                    self.currentImageIndex = 0
+                }
+            case .right:
+                self.currentImageIndex += 1
+                
+                if self.currentImageIndex < 0 {
+                    self.currentImageIndex = self.imagesArray.count - 1
+                }
+            }
+        }
     
 }
 
+}
